376ad6315d936053155ccd097e35d1abe1984d92
skyepub <skytree@skyepub.net>
2022-07-06 10:42:12 +0900

    set

--- essense01.py
+++ essense01.py
@@ -34,6 +34,7 @@ b = 43
 c = plus(a,b)
 print(c)
 
+# list 
 aa = []
 aa.append(0)
 aa.append(1)
@@ -45,3 +46,60 @@ for i in range(3,10):
 print(aa)
 
 print(aa[4:7])
+
+aa.insert(1,10000)
+print(aa)
+
+aa.sort()
+print(aa)
+
+aa.reverse()
+print(aa)
+
+
+# dictionary 
+info = {}
+info['name'] = "Jiung"
+info['language']="korean"
+info['city']="busan"
+info['secret']='secret_12kdk3kd'
+
+print(info['name'])
+print(info)
+
+del(info['secret'])
+print(info)
+
+info['name'] = "Jiung Heo"
+print(info)
+
+print(info['language'])
+print(info.get('language'))
+
+keys = info.keys()
+print(keys)
+values = info.values()
+print(values)
+
+for k in keys:
+    print(k)
+
+for v in values:
+    print(v)
+
+# set
+sa = {1,2,3,4,5,6}
+sb = {4,5,6,7,8}
+
+print(sa)
+print(sb)
+
+sr1 = sa & sb
+sr2 = sa | sb
+sr3 = sa - sb
+sr4 = sa ^ sb
+
+print(sr1)
+print(sr2)
+print(sr3)
+print(sr4)
