cbae112721f3181b4f0c9db2b17c38839be45086
skyepub <skytree@skyepub.net>
2022-07-06 11:25:34 +0900

    tuple

--- essense01.py
+++ essense01.py
@@ -103,3 +103,93 @@ print(sr1)
 print(sr2)
 print(sr3)
 print(sr4)
+
+sr1 = sa.intersection(sb)
+sr2 = sa.union(sb)
+sr2 = sa.difference(sb)
+sr2 = sa.symmetric_difference(sb)
+
+print(sr1)
+print(sr2)
+print(sr3)
+print(sr4)
+
+# tuple
+tt1 = (10,20,30)
+print(tt1)
+
+tt2 = 10,20,30
+print(tt2)
+
+# 항목이 하나인 경우 ,를 붙여서 튜플임을 표시
+tt3 = 10,
+tt4 = (20,)
+
+print(tt1[2])
+print(tt2[1])
+
+# 튜플은 내부의 값을 바꿀수는 없으나 간단한 연산 결과는 돌려줄 수 있다. 
+tta = ('a','b')
+ttb = ('c','c')
+
+tr1 = tta + ttb
+print(tr1)
+
+tr2 = tr1 * 3
+print(tr2)
+
+
+# list, tuple 상호변혼
+la = list(tta)
+print(la)
+la.append('k')
+print(la)
+tca = tuple(la)
+print(tca)
+
+
+# 문자열
+str1 = "we are the world"
+str2 = "we are the children."
+strb = str1[3:6]
+print(strb)
+strb = str1[7:]
+print(strb)
+
+stra = str1 + ", " + str2
+print(stra)
+strc = str1 * 3
+print(strc)
+
+print(len(str1))
+
+countFound = stra.count('we')
+print(countFound) 
+
+pos1 = stra.find("the")
+print(pos1)
+
+pos2 = stra.rfind("the")
+print(pos2)
+
+# index는 없을 경우 오류발생
+
+index1 = stra.index("the")
+print(index1)
+
+index2 = stra.rindex("the")
+print(index2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
